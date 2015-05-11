77bfaabccceef3a31f07dfbe00226f7f68d21f35
Fabien Boucher <fabien.boucher@enovance.com>
2015-05-11 18:27:46 +0000

    WIP - add fedora support

--- prepare/prepare.sh
+++ prepare/prepare.sh
@@ -8,7 +8,13 @@ WORKDIR=/var/cache/int_tests
 BASE_IMAGES=$WORKDIR/bases
 BASE_LXC=/var/lib/lxc
 
-BASE_DISTS="precise trusty"
+BASE_DISTS="fedora:21 ubuntu:precise ubuntu:trusty"
+
+function get_os_release {
+  local dist=$1
+  distrib=$(echo $dist | cut -d: -f1)
+  release=$(echo $dist | cut -d: -f2)
+}
 
 function install_req {
   sudo yum install -y lxc lxc-devel lxc-templates git python-pip bridge-utils which gcc python-devel openssl
@@ -34,15 +40,18 @@ function setup_overlay {
 
 function get_dist {
   local dist=$1
-  DIB_RELEASE=$dist $DIB -a amd64 -t tar -o ubuntu-amd64 ubuntu
-  sudo mv ubuntu-amd64.tar $BASE_IMAGES/${1}.tar
-  [ -d $BASE_IMAGES/$1 ] && sudo rm -Rf $BASE_IMAGES/$1
-  sudo mkdir $BASE_IMAGES/$1 && sudo tar -xf $BASE_IMAGES/${1}.tar -C $BASE_IMAGES/$1
+  get_os_release $dist
+  DIB_RELEASE=$release $DIB -a amd64 -t tar -o temp $distrib
+  sudo mv temp.tar $BASE_IMAGES/${release}.tar
+  rm -Rf temp.d
+  [ -d $BASE_IMAGES/$release ] && sudo rm -Rf $BASE_IMAGES/$release
+  sudo mkdir $BASE_IMAGES/$release && sudo tar -xf $BASE_IMAGES/${release}.tar -C $BASE_IMAGES/$release
 }
 
 function adapt_dist {
   local dist=$1
-  pushd $BASE_IMAGES/$1
+  get_os_release $dist
+  pushd $BASE_IMAGES/$release
   sudo rm -Rf dev
   # LXC will populate dev/ at startup
   sudo mkdir sys proc dev
